bb0f58a86054def266103ac0817032d887d16600
SulaimanAshimzhanov <sulajmanasimzanov@gmail.com>
2025-02-16 23:53:48 +0600

    I layout and adapt the contact page and write the logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 import ExpandableContentCollection from "./ExpandableContent.js";
 import Header from "./Header.js";
+import SelectCollection from "./Select.js";
 import TabsCollection from "./Tabs.js";
 import VideoPlayerCollection from "./VideoPlayer.js";
+import InputMaskCollection from "./inputMask.js";
 
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
 new ExpandableContentCollection()
+new InputMaskCollection()
+new SelectCollection()
